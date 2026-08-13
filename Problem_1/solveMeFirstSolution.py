954c4675d7b1d526643f18bb5dac12f83335fc7f
UnicornDad8 <cecibenitezca@gmail.com>
2026-08-13 14:15:18 -0300

    Add problem 1, and solution to 'solveMeFirst'

---
+++ Problem_1/solveMeFirstSolution.py
@@ -0,0 +1,19 @@
+# Completa la funcion "solveMeFirst" para computar la suma de dos enteros.
+# Ejemplo:
+# a = 2
+# b = 3
+# output devuelve 5
+# -----------------------------------------------------------------------------
+# int a: (primer valor)
+# int b: (segundo valor)
+# Output:
+# devuelve la suma de a más b.
+# -----------------------------------------------------------------------------
+# Restricciones:
+# 1 <= a, b <= 1000
+
+def sum_a_and_b(a, b):
+    return a + b
+
+
+print(sum_a_and_b(2, 3))
