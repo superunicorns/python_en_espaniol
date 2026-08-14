9d9f1dce8151febf87d46487f70c82dc463dd77b
UnicornDad8 <cecibenitezca@gmail.com>
2026-08-14 18:51:25 -0300

    Add 'diagonalDifference' problem and solution

---
+++ Problem_5/diagonalDifferenceSolution.py
@@ -0,0 +1,39 @@
+# Dada una matriz cuadrada, calcula la diferencia en valor absoluto de
+# la suma de los elementos de sus diagonales.
+# ----------------------------------------------------------------------
+# Ejemplo:
+# tenemos la siguiente matriz abajo de esta linea
+# 1    2    3
+# 4    5    6
+# 9    8    9
+# la diagonal de izquierda a derecha (diagonal principal) =
+# 1 + 5 + 9 = 15
+# la diagonal de derecha a izquierda (anti diagonal) =
+# 3 + 5 + 9 = 17
+# la diferencia absoluta es |15 - 17| = 2
+# -----------------------------------------------------------------------
+# Parametros:
+# int arr[n][m]: arreglo 2D de enteros
+# Salida:
+# int: la diferencia absoluta de la suma de las diagonales.
+# -----------------------------------------------------------------------
+# Restricciones:
+# -100 <= arr[i][j] <= 100
+
+def diagonal_difference(arr):
+    n = len(arr)  # longitud de la matriz
+    left_to_right = 0  # suma de los elementos de la diagonal de izquierda a derecha
+    right_to_left = 0  # suma de los elementos de la diagonal de derecha a izquierda
+
+    # recorremos la matriz con un for loop
+    for i in range(n):
+        left_to_right += arr[i][i]  # sumamos los elementos de la diagonal principal
+        # Obs: fila x columna = [i][n - i - 1] = [i][n - (i + 1)]
+        right_to_left += arr[i][n - (i + 1)]  # sumamos los elementos de la antidiagonal
+
+    # para devolver un valor absoluto usamos la funcion abs()
+    return abs(left_to_right - right_to_left)
+
+
+print(diagonal_difference([[1, 2, 3], [4, 5, 6], [9, 8, 9]]))  # salida: 2
+print(diagonal_difference([[11, 2, 4], [4, 5, 6], [10, 8, -12]]))  # salida: 15
