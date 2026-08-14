2536bf1570ec10532fde601c1cc834fd1f96ef54
UnicornDad8 <cecibenitezca@gmail.com>
2026-08-14 13:40:17 -0300

    Add 'aVeryBigSum' problem and solution

---
+++ Problem_4/aVeryBigSum.py
@@ -0,0 +1,22 @@
+# en este desafio, tu tienes que calcular e imprimir la suma de los
+# elementos de un arreglo.
+# (considerando que algunos elementos puedan ser muy grandes).
+# -----------------------------------------------------------------------
+# Parametros:
+# int ar[n]: arreglo de enteros
+# long: la suma de los elementos del arreglo
+# -----------------------------------------------------------------------
+# Ejemplo:
+# ar[n] = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+# Salida:
+# devuelve 5000000015
+# ------------------------------------------------------------------------
+# Restricciones:
+# 1 <= n <= 10
+# 0 <= ar[i] <= (10)^10
+
+def array_sum(ar):
+  # escribe tu codigo aqui en esta linea
+
+
+print(array_sum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]))
---
+++ Problem_4/aVeryBigSumSolution.py
@@ -0,0 +1,31 @@
+# en este desafio, tu tienes que calcular e imprimir la suma de los
+# elementos de un arreglo.
+# (considerando que algunos elementos puedan ser muy grandes).
+# -----------------------------------------------------------------------
+# Parametros:
+# int ar[n]: arreglo de enteros
+# long: la suma de los elementos del arreglo
+# -----------------------------------------------------------------------
+# Ejemplo:
+# ar[n] = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+# Salida:
+# devuelve 5000000015
+# ------------------------------------------------------------------------
+# Restricciones:
+# 1 <= n <= 10
+# 0 <= ar[i] <= (10)^10
+
+def array_sum(ar):
+    # necesitamos una variable para ir cargando la suma de elementos del arreglo
+    sum = 0
+
+    # recorremos el arreglo, con un elemento en cada iteracion
+    for el in ar:
+        # sumamos el primer elemento, luego el segundo, y asi hasta terminar el arreglo
+        sum += el
+
+    # devolvemos la suma de todos los elementos del arreglo
+    return sum
+
+
+print(array_sum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]))
